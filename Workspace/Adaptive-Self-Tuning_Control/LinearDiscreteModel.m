e413ed4bfb4dcaec605a0fe0726bf36e623f89a4
iusti <iustinmarkis23@gmail.com>
2023-06-20 23:48:50 +0300

    New class for discrete time model simulation

---
+++ Workspace/Adaptive-Self-Tuning_Control/LinearDiscreteModel.m
@@ -0,0 +1,36 @@
+classdef LinearDiscreteModel   
+    properties
+        theta
+        phi
+        nd
+        na
+        buffer
+    end
+    
+    methods
+        function obj = LinearDiscreteModel(num ,den, nd)
+            obj.theta = [den(2:end) num]';
+            if den(1) ~= 1
+                obj.theta = obj.theta/den(1);
+            end
+            obj.phi = zeros(length(obj.theta), 1);
+            obj.nd = nd;
+            obj.na = length(den(2:end));
+            obj.buffer = zeros(nd, 1);
+        end
+        
+        function [obj, output] = forward(obj, input)
+            if obj.nd ~= 0
+                obj.phi(obj.na + 1:end) = [obj.buffer(end); obj.phi(obj.na + 1:end - 1)];
+                obj.buffer = [input; obj.buffer(1:end - 1)];
+            else
+                obj.phi(obj.na + 1:end) = [input; obj.phi(obj.na + 1:end - 1)];
+            end
+            
+            output = obj.theta'*obj.phi;
+            
+            obj.phi(1:obj.na) = [-output; obj.phi(1:obj.na - 1)];
+        end
+    end
+end
+
